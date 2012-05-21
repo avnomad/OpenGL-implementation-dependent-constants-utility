8e433a4bdec4cff0d1e6c23d5452c08b48983dac
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2012-05-22 02:46:39 +0300

    Made some changes to the copyright notices to match the format of other projects.

--- src/implementation constants/main-base.cpp
+++ src/implementation constants/main-base.cpp
@@ -1,18 +1,19 @@
+//	Copyright (C) 2010-2012 Vaptistis Anogeianakis <el05208@mail.ntua.gr>
 /*
- *	Copyright (C) 2010-2012  Anogeianakis Vaptistis
+ *	This file is part of Implementation-dependent Constants.
  *
- *	This program is free software: you can redistribute it and/or modify
+ *	Implementation-dependent Constants is free software: you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ *	Implementation-dependent Constants is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *	You should have received a copy of the GNU General Public License
+ *	along with Implementation-dependent Constants.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #pragma warning(disable : 4996)	// disable deprecation warnings for Visual Studio.
@@ -80,7 +81,7 @@ int main(int argc, char **argv)
 	glGetIntegerv(GL_NUM_COMPRESSED_TEXTURE_FORMATS,&nTextureFormats);
 	cout << left;
 
-	cout << setw(10) << ' ' << "Copyright (C) 2010-2012  Anogeianakis Vaptistis\n\n";
+	cout << setw(8) << ' ' << "Copyright (C) 2010-2012  Vaptistis Anogeianakis\n\n";
 	cout << setw(6) << ' ' << "This program comes with ABSOLUTELY NO WARRANTY.\n";
     cout << setw(6) << ' ' << "This is free software, and you are welcome to\n";
 	cout << setw(6) << ' ' << "redistribute it under certain conditions.\n";
